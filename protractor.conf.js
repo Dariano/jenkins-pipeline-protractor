23c32fcc7fc8dc0abdfce42d055dcce9ec542a1c
Soares <darianosoares@hotmail.com>
2016-11-30 01:02:32 -0200

    configurando

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,11 @@
 exports.config = {
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  restartBrowserBetweenTests: true,
   directConnect: true,
-  specs: ['todo-spec.js']
+  specs: ['todo-spec.js'],
+  capabilities : {
+   'browserName': 'chrome' 
+  },
+  chromeOnly: true,
+  framework: 'jasmine2'
 };
