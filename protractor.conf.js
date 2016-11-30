42c2cdaa782c20c841fff5ef9ef3a46a6db0d22d
Soares <darianosoares@hotmail.com>
2016-11-30 01:19:28 -0200

    removendo directChrome

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,11 +1,9 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   restartBrowserBetweenTests: true,
-  directConnect: true,
   specs: ['todo-spec.js'],
   capabilities : {
    'browserName': 'chrome' 
   },
-  chromeOnly: true,
   framework: 'jasmine2'
 };
