c90c9ef2d03529fbc078f2bcce0d94e7b617bc3b
Soares <darianosoares@hotmail.com>
2016-11-30 02:37:01 -0200

    remove restartBrowserBetweenTests

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,7 +2,6 @@ exports.config = {
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   //seleniumServerJar: './node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-3.0.1.jar',
   directConnect: true,
-  restartBrowserBetweenTests: true,
   specs: ['todo-spec.js'],
   capabilities : {
    'browserName': 'chrome'
