7bddeca8e00bbdef6a35ef625d015e7c5ac40d9d
Soares <darianosoares@hotmail.com>
2016-11-30 01:23:15 -0200

    comentario seleniumAddress

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,5 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
   restartBrowserBetweenTests: true,
   specs: ['todo-spec.js'],
   capabilities : {
