9efe6e0ef4136ec37fe4f1a841d60606298e3373
Soares <darianosoares@hotmail.com>
2016-12-01 09:34:42 -0200

    add directConnect=true

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,7 @@
 exports.config = {
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   //seleniumServerJar: './node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-3.0.1.jar',
-  //directConnect: true,
+  directConnect: true,
   specs: ['todo-spec.js'],
   capabilities : {
     'browserName': 'chrome'
