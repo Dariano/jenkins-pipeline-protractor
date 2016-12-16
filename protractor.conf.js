9e55c7bfdb6e226e8a91d09971cd6d1fb88ea967
Soares <darianosoares@hotmail.com>
2016-12-16 12:25:55 -0200

    seleniumServerJar

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,7 @@
 exports.config = {
   //seleniumAddress: 'http://localhost:4444/wd/hub',
-  //seleniumServerJar: './node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-3.0.1.jar',
-  directConnect: true,
+  seleniumServerJar: './node_modules/protractor/node_modules/webdriver-manager/selenium//selenium-server-standalone-2.53.1.jar',
+  //directConnect: true,
   specs: ['todo-spec.js'],
   capabilities : {
     'browserName': 'chrome'
