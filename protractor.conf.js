477f283638d745132ed6a112d0e1cd80e3e1d64b
Soares <darianosoares@hotmail.com>
2017-03-21 13:55:46 -0300

    teste

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
-  seleniumServerJar: './node_modules/protractor/node_modules/webdriver-manager/selenium//selenium-server-standalone-2.53.1.jar',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+//   seleniumServerJar: './node_modules/protractor/node_modules/webdriver-manager/selenium//selenium-server-standalone-2.53.1.jar',
   //directConnect: true,
   specs: ['todo-spec.js'],
   capabilities : {
